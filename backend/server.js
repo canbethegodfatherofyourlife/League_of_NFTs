a88edf643040f4f6bebe185807c22e34eb4e40d5
Arka <arkadatta2002@gmail.com>
2022-08-15 11:10:51 +0530

    "build"

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const cors = require("cors");
+const path = require("path");
 
 dotenv.config();
 connectDB();
@@ -29,6 +30,21 @@ app.use("/auction", require("./routes/auction"));
 app.use("/leaderboard", require("./routes/leaderboard"));
 app.use("/trading", require("./routes/trading"));
 app.use("/profile", require("./routes/profile"));
+
+// ------------------------------------Deployment --------------------------------
+
+const __dirname1 = path.resolve()
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname1,'/frontend/build')))
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname1,'frontend','build','index.html'));
+  })
+
+}else{
+  app.send("API is Running Successfully!")
+}
+
+// ------------------------------------Deployment --------------------------------
 
 const PORT = process.env.PORT || 3008;
 app.listen(PORT, console.log(`Server Started on PORT ${PORT}`));
